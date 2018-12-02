95c06857c5026ec28541a6b0a6669686a46f8e39
Cristian Garay <cgaray@gmail.com>
2018-12-01 17:57:28 -0800

    Creating ComputeSizeOfBitArray method

--- BloomFilter/BloomFilter/MyBloomFilter.cs
+++ BloomFilter/BloomFilter/MyBloomFilter.cs
@@ -7,5 +7,11 @@ namespace BloomFilter
         public MyBloomFilter()
         {
         }
+
+        public int ComputeSizeOfBitArray(int numberOfElements, double falsePositiveProb)
+        {
+            int sizeOfBitArray = (int)Math.Ceiling(-1 * (numberOfElements * Math.Log(falsePositiveProb)) / (Math.Pow(Math.Log(2), 2)));
+            return sizeOfBitArray;
+        }
     }
 }
