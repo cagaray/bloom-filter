5d0f262f252753f720597628661ad3ce6daae8a4
Cristian Garay <cgaray@gmail.com>
2018-12-01 18:26:51 -0800

    Adding test when no elements

--- BloomFilter/BloomFilter.UnitTests/MyBloomFilterTests.cs
+++ BloomFilter/BloomFilter.UnitTests/MyBloomFilterTests.cs
@@ -6,7 +6,7 @@ namespace BloomFilter.UnitTests
     public class MyBloomFilterTests
     {
         [Fact]
-        public void Test1()
+        public void ComputeSizeOfBitArray_CorrectParameters_ReturnCorrectResult()
         {
             int numberOfElements = 1000;
             double falsePositiveProb = 0.01;
@@ -15,6 +15,18 @@ namespace BloomFilter.UnitTests
             int sizeOfBitArray = filter.ComputeSizeOfBitArray(numberOfElements, falsePositiveProb);
 
             Assert.Equal(9586, sizeOfBitArray);
+        }
+
+        [Fact]
+        public void ComputeSizeOfBitArray_ZeroElements_ReturnZero()
+        {
+            int numberOfElements = 0;
+            double falsePositiveProb = 0.1;
+            MyBloomFilter filter = new MyBloomFilter();
+
+            int sizeOfBitArray = filter.ComputeSizeOfBitArray(numberOfElements, falsePositiveProb);
+
+            Assert.Equal(0, sizeOfBitArray);
         }
     }
 }
