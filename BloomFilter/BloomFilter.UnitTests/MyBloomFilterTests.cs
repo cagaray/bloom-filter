196eb2be5b39a5c25567dbb3eaa5e38e7a7fe337
Cristian Garay <cgaray@gmail.com>
2018-12-01 18:57:22 -0800

    Adding out of range checking for false positive prob

--- BloomFilter/BloomFilter.UnitTests/MyBloomFilterTests.cs
+++ BloomFilter/BloomFilter.UnitTests/MyBloomFilterTests.cs
@@ -6,7 +6,7 @@ namespace BloomFilter.UnitTests
     public class MyBloomFilterTests
     {
         [Fact]
-        public void ComputeSizeOfBitArray_CorrectParameters_ReturnCorrectResult()
+        public void ComputeSizeOfBitArray_CorrectParameters_ReturnsCorrectResult()
         {
             int numberOfElements = 1000;
             double falsePositiveProb = 0.01;
@@ -27,6 +27,42 @@ namespace BloomFilter.UnitTests
             int sizeOfBitArray = filter.ComputeSizeOfBitArray(numberOfElements, falsePositiveProb);
 
             Assert.Equal(0, sizeOfBitArray);
+        }
+
+        [Fact]
+        public void ComputeSizeOfBitArray_ZeroFalsePositiveProb_ThrowsException()
+        {
+            int numberOfElements = 1000;
+            double falsePositiveProb = 0.0;
+            MyBloomFilter filter = new MyBloomFilter();
+
+            Action act = () => filter.ComputeSizeOfBitArray(numberOfElements, falsePositiveProb);
+
+            Assert.Throws<ArgumentOutOfRangeException>(act);
+        }
+
+        [Fact]
+        public void ComputeSizeOfBitArray_NegativeFalsePositiveProb_ThrowsException()
+        {
+            int numberOfElements = 1000;
+            double falsePositiveProb = -0.1;
+            MyBloomFilter filter = new MyBloomFilter();
+
+            Action act = () => filter.ComputeSizeOfBitArray(numberOfElements, falsePositiveProb);
+
+            Assert.Throws<ArgumentOutOfRangeException>(act);
+        }
+
+        [Fact]
+        public void ComputeSizeOfBitArray_GreaterThanOneFalsePositiveProb_ThrowsException()
+        {
+            int numberOfElements = 1000;
+            double falsePositiveProb = 1.1;
+            MyBloomFilter filter = new MyBloomFilter();
+
+            Action act = () => filter.ComputeSizeOfBitArray(numberOfElements, falsePositiveProb);
+
+            Assert.Throws<ArgumentOutOfRangeException>(act);
         }
     }
 }
--- BloomFilter/BloomFilter/MyBloomFilter.cs
+++ BloomFilter/BloomFilter/MyBloomFilter.cs
@@ -10,6 +10,8 @@ namespace BloomFilter
 
         public int ComputeSizeOfBitArray(int numberOfElements, double falsePositiveProb)
         {
+            if (falsePositiveProb <= 0.0 || falsePositiveProb >= 1.0)
+                throw new ArgumentOutOfRangeException();
             int sizeOfBitArray = (int)Math.Ceiling(-1 * (numberOfElements * Math.Log(falsePositiveProb)) / (Math.Pow(Math.Log(2), 2)));
             return sizeOfBitArray;
         }
