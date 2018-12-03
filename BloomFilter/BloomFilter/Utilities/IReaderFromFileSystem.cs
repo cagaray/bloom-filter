adad399f8c5d665b9a98dc590c1b81837d3e582c
Cristian Garay <cgaray@gmail.com>
2018-12-02 17:53:50 -0800

    Adding async text reader from file

---
+++ BloomFilter/BloomFilter/Utilities/IReaderFromFileSystem.cs
@@ -0,0 +1,9 @@
+﻿using System.Threading.Tasks;
+
+namespace BloomFilter.Utilities
+{
+    public interface IReaderFromFileSystem
+    {
+        Task<string> GetContentsFromFileAsync(string path);
+    }
+}
---
+++ BloomFilter/BloomFilter/Utilities/TxtReaderFromFileSystem.cs
@@ -0,0 +1,18 @@
+﻿using System.IO;
+using System.Threading.Tasks;
+
+namespace BloomFilter.Utilities
+{
+    public class TxtReaderFromFileSystem : IReaderFromFileSystem
+    {
+        public TxtReaderFromFileSystem()
+        {
+        }
+
+        public async Task<string> GetContentsFromFileAsync(string path)
+        {
+            using (var reader = File.OpenText(path))
+                return await reader.ReadToEndAsync();
+        }
+    }
+}
