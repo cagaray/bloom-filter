770c5f711ce9a4002a7e50d36cb7f12782a4d4d5
Cristian Garay <cgaray@gmail.com>
2018-12-03 22:56:29 -0800

    Minor Changes to Console App

--- BloomFilter/SpellCheckerApp/Command.cs
+++ BloomFilter/SpellCheckerApp/Command.cs
@@ -1,12 +1,12 @@
 ﻿using System;
+using System.Collections.Generic;
+
 namespace SpellCheckerApp
 {
     public class Command
     {
         public Verbs Verb { get; set; }
-        public string Param { get; set; }
-        public string Param2 { get; set; }
-        public string Param3 { get; set; }
+        public List<string> Params { get; set; }
 
         public enum Verbs
         {
@@ -19,18 +19,17 @@ namespace SpellCheckerApp
 
         public Command(string command)
         {
+            Params = new List<string>();
             string[] commands = command.Split(' ');
-            if(command.Length != 2)
-                Verb = Verbs.error;
             switch (commands[0])
             {
                 case "-l":
                     if (commands.Length >= 4)
                     {
                         Verb = Verbs.load;
-                        Param = commands[1];
-                        Param2 = commands[2];
-                        Param3 = commands[3];
+                        Params.Add(commands[1]);
+                        Params.Add(commands[2]);
+                        Params.Add(commands[3]);
                     }
                     else
                         Verb = Verbs.error;
@@ -39,7 +38,7 @@ namespace SpellCheckerApp
                     if (commands.Length >= 2)
                     {
                         Verb = Verbs.check;
-                        Param = commands[1];
+                        Params.Add(commands[1]);
                     }
                     else
                         Verb = Verbs.error;
@@ -51,7 +50,7 @@ namespace SpellCheckerApp
                     Verb = Verbs.exit;
                     break;
                 default:
-                    Verb = Verbs.exit;
+                    Verb = Verbs.error;
                     break;
             }
         }
@@ -59,7 +58,7 @@ namespace SpellCheckerApp
         public static void PrintHelp()
         {
             Console.WriteLine("Usage:\n");
-            Console.WriteLine("-l <file path> <number of elements> <false positive prob>\n");
+            Console.WriteLine("-l <file path> <number of elements> <false positive prob>: File path with words and parameters for Bloom Filter\n");
             Console.WriteLine("-c <word>: Check for a word in the dictionary. Need to load dictionary file first (-l).\n");
             Console.WriteLine("-h: Show help\n");
             Console.WriteLine("-e: Exit the application\n");
